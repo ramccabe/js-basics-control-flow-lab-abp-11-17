798a4715f3ad3e9786e09bdc37c106ae68975553
Robert McCabe <robertadammccabe@gmail.com>
2017-11-12 01:25:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function scuberGreetingForFeet() {
-   if scuberGreetingForFeet() <= 400 
+function scuberGreetingForFeet(){
+   if scuberGreetingForFeet() === <= 400
    console.log("This one is on me!");
 }
