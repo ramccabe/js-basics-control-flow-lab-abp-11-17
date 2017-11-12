118c5de65aece43bc43f6b5fc39d2e423aa8cb84
Robert McCabe <robertadammccabe@gmail.com>
2017-11-12 02:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-function scuberGreetingForFeet(distance){
-   if (distance <= 400){
-     return 'This one is on me!'
-   }
+function scuberGreetingForFeet(distance)
+if (distance <= 400){
+  return 'This one is on me!'
+}
