35b708837205abec77b89ae9d0541b0ac95c5fbb
Robert McCabe <robertadammccabe@gmail.com>
2017-11-12 02:22:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function scuberGreetingForFeet(distance){
-if (distance <= 400){
-  return 'This one is on me!'}
-} else if (distance > 2000){
-    return 'I will gladly take your thirty bucks.'
+  if (distance <= 400){
+      return 'This one is on me!'}
+  }   else if ((distance > 2000) && (distance < 2500)){
+      return 'I will gladly take your thirty bucks.'
+  }    else if (distance > 2500){
+      return 'No can do.'
+  }
 }
