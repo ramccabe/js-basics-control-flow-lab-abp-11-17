d9e541cc5a22346b134b3cf5aadb1121a794e008
Robert McCabe <robertadammccabe@gmail.com>
2017-11-12 02:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ function scuberGreetingForFeet(distance){
   }
 }
 
-function ternaryCheckCity(city){
-  if (city === "NYC"){
-    return 'Ok, sounds good.'
-  } else return 'No go.'
-}
+return (city === "NYC") ? "Ok, sounds good." : "No go."
